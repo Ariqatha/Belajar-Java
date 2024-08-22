4d6547d610b52d2c4419ba8611c7f7b7cb1d2f3b
Ariqatha <129348122+Ariqatha@users.noreply.github.com>
2024-08-22 11:11:20 +0700

    Add files via upload

---
+++ Modul 1 neatbeans/src/modul/pkg1/neatbeans/Modul1Neatbeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul.pkg1.neatbeans;
+
+/**
+ *
+ * @author Ariq Athallah H
+ */
+public class Modul1Neatbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
